6aee7ce8aa82284df40e5b1a788bf631709b5681
Matt-Young <youngsanger@gmail.com>
2012-01-20 14:03:18 -0800

    Binds definitions header

---
+++ src/binds.h
@@ -0,0 +1,11 @@
+// Bind methods which are called just before an installed statement runs
+#define BIND_DEFAULT 1
+#define BIND_GRAPH 2
+#define BIND_OTHER 3
+#define BIND_SELF 3
+#define BIND_TRIPLE 5
+#define BIND_SCHEMA 6
+#define BIND_GRAPH_ROW 0
+#define BIND_GRAPH_START 1
+#define BIND_GRAPH_END 2
+PGRAPH *LIST(int i);
